48de38f1a1d02b8f9d9c25eca0ab54261e7e93ea
Chad ONeal <chadoneal3@gmail.com>
2023-04-07 19:59:48 -0400

    day-19

---
+++ src/app/shared/models/dialog-data.interface.ts
@@ -0,0 +1,15 @@
+/*
+============================================
+; Title: dialog-data.interface.ts
+; Author: Professor Krasso
+; Modified by: Chad ONeal
+; Date: 04/07/2023
+; Description: for nodebucket
+============================================
+*/
+
+// export statement
+export interface DialogData {
+  header: string;
+  body: string;
+}
--- src/app/shared/models/dialogue-data.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface DialogueData {
-  header: string;
-  body: string;
-}
